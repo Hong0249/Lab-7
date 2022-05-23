9198d63fd48f908e5abfae0eba8d7b7f8a5e70d0
Yak Xuan Hong <b022010002@student.utem.edu.my>
2022-05-23 21:08:28 +0800

    latest

--- main-api.test.js
+++ main-api.test.js
@@ -1,76 +1,66 @@
 const supertest = require('supertest');
 const request = supertest('http://localhost:3000');
 
-describe('Express Route Test', function () {
-    
-	it('POST', async () => {
-		return request
-			.post('/create')
-			.send({title: 'Petter Pan', isbn: "51651118", author: "A.W. Toomer"})
-			.expect('Content-Type', /json/)
-			.expect(200).then(res => {
-				expect(res.body).toEqual({
-                    isbn: "51651118", 
-                    title: 'Petter Pan',
-                    author: "A.W. Toomer"
-				})
-			})
-	});
-
-	it('POST', async () => {
-		return request
-			.post('/create')
-			.send({title: 'Harry Petter', isbn: "987654321", author: "Steven Dwager"})
-			.expect('Content-Type', /json/)
-			.expect(200).then(res => {
-				expect(res.body).toEqual({
-                    isbn: "987654321", 
-                    title: 'Harry Petter',
-                    author: "Steven Dwager"
-				})
-			})
-	});
+const Book = [
+	{
+		title : "Computer Networks: An Open Source Approach",
+		author : "Lin, Ying-Dar - Hwang, Ren-Hung - Baker, Fred",
+		isbn : "978-0-0733-7624-0",
+	},
+	{
+		title : "Practical Packet Analysis, 3E: Using Wireshark to Solve Real-World Network Problems",
+		author : "Sanders, Chris",
+		isbn : "978-1-5932-7802-1",
+	},
+	{
+		title : "802.11ac: A Survival Guide: Wi-Fi at Gigabit and Beyond",
+		author : "J.K. Rowling",
+		isbn : "978-1-1156-4314-9",
+	},
+]
 	
-	it('POST', async () => {
-		return request
-			.post('/create')
-			.send({title: 'Harry Potter', isbn: "123456789", author: "J.K. Rowling"})
-			.expect('Content-Type', /json/)
-			.expect(200).then(res => {
-				expect(res.body).toEqual({
-                    isbn: "123456789", 
-                    title: 'Harry Potter',
-                    author: "J.K. Rowling"
+describe('Express Route Test', function () {
+    for(let i = 0; i < Book.length; i++) {
+		it('POST', async () => {
+			return request
+				.post('/create')
+				.send({title: Book[i].title, isbn: Book[i].isbn, author: Book[i].author})
+				.expect('Content-Type', /json/)
+				.expect(200).then(res => {
+					expect(res.body).toEqual({
+						isbn: Book[i].isbn, 
+						title: Book[i].title,
+						author: Book[i].author
+					})
 				})
-			})
-	});
+		});
+	}	
 
 	it('GET', async () => {
 		return request
 			.get('/read')
-			.send({title: 'Harry Potter'})
+			.send({title: 'Computer Networks: An Open Source Approach'})
 			.expect('Content-Type', /json/)
 			.expect(200).then(res => {
 				expect(res.body).toEqual({
-                    title: 'Harry Potter',
-                    isbn: "123456789", 
-                    author: "J.K. Rowling"
+                    title: 'Computer Networks: An Open Source Approach',
+                    isbn: "978-0-0733-7624-0", 
+                    author: "Lin, Ying-Dar - Hwang, Ren-Hung - Baker, Fred"
 				})
 			})
 	});
 
-
 	it('DELETE', async () => {
 		return request
 			.delete('/delete')
-			.send({title: 'Harry Petter'})
+			.send({title: 'Practical Packet Analysis, 3E: Using Wireshark to Solve Real-World Network Problems'})
 			.expect('Content-Type', /json/)
 			.expect(200).then(res => {
 				expect(res.body).toEqual({
 					message: expect.any(String),
-					title: 'Harry Petter',
-                    isbn: "987654321", 
-                    author: "Steven Dwager"
+					title: 'Practical Packet Analysis, 3E: Using Wireshark to Solve Real-World Network Problems',
+                    isbn: "978-1-5932-7802-1", 
+                    author: "Sanders, Chris"
 				});
 			})
 	})
@@ -78,16 +68,28 @@ describe('Express Route Test', function () {
 	it('PATCH', async () => {
 		return request
 			.patch('/update')
-			.send({title: 'Harry Potter', isbn: "6481561561", author: "Mc Craig"})
+			.send({title: '802.11ac: A Survival Guide: Wi-Fi at Gigabit and Beyond', isbn: "978-1-4493-4314-9", author: "Gast, Matthew S"})
 			.expect('Content-Type', /json/)
 			.expect(200).then(res => {
 				expect(res.body).toEqual({
 					message: expect.any(String),
-                    title: 'Harry Potter',
-                    isbn: "123456789 to 6481561561", 
-                    author: "J.K. Rowling to Mc Craig"
+                    title: '802.11ac: A Survival Guide: Wi-Fi at Gigabit and Beyond',
+                    isbn: "978-1-1156-4314-9 to 978-1-4493-4314-9", 
+                    author: "J.K. Rowling to Gast, Matthew S"
 				});
 			})
 
 	 })
+
+	 it('GET', async () => {
+		return request
+			.get('/getall')
+			.send({})
+			.expect('Content-Type', /json/)
+			.expect(200).then(res => {
+				expect(res.body).toEqual({
+				})
+			})
+	});
+
 })
